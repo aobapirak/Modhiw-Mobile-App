c65896cb411589c2a9fee3a6fb890e4fc951d4fc
aobapirak <apiraksenarak25808@gmail.com>
2022-11-01 02:52:29 +0700

    navigate page

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
+const Stack = createNativeStackNavigator();
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import RestaurantInfo from "./screens/RestaurantInfo";
+import Homepage from "./screens/Homepage";
+import LogIn from "./screens/LogIn";
+import FoodInfo from "./screens/FoodInfo";
+// import ConfirmBookIcon from "./screens/ConfirmBookIcon";
 
-export default function App() {
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View, Text, Pressable, TouchableOpacity } from "react-native";
+
+const App = () => {
+  const [hideSplashScreen, setHideSplashScreen] = React.useState(true);
+  const SplashScreen = () => <View />;
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer>
+        {hideSplashScreen ? (
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+              name="LogIn"
+              component={LogIn}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Homepage"
+              component={Homepage}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="RestaurantInfo"
+              component={RestaurantInfo}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="FoodInfo"
+              component={FoodInfo}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        ) : (
+          <SplashScreen />
+        )}
+        
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
